a7d1723b999f5a40efe243f7b4f6129f89c4f314
Yvonne Tashy <tashyvie@gmail.com>
2025-10-07 21:27:11 +0300

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+        int a = 3;
+        float b = 5.1f;
+        double c = 4.9;
+        String name = ("Tashy");
+        System.out.println("Hi"+ name);
+        System.out.println("You got a :" + c +"GPA");
+        System.out.println("You made it to the top "+ b +"%");
+        System.out.println("You are number "+ a);
+    }
+}
