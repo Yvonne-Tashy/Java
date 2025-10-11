f8c9965e02d3734a2cc374ebcc6ff2d4a556eff8
Yvonne Tashy <tashyvie@gmail.com>
2025-10-12 01:39:43 +0300

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.pow;
+
+public class Operators {
+public static void main(String[] args){
+    int x=5;
+    int y=3;
+    System.out.println(x+y);//Addition
+    System.out.println(x/y);//division
+    System.out.println(x-y);//Subtraction
+    System.out.println(x%y);//Modulus
+    System.out.println(Math.pow(x,y));//exponentiation
+}
+}
